669b396e2c57b1162df4f615bb80d7681b245efe
noureddine <nounoubensebia96@gmail.com>
2018-02-06 21:48:12 +0100

    initial commit

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -20,6 +20,7 @@ class CreatePasswordResetsTable extends Migration
             $table->engine = 'InnoDB';
             $table->charset = 'utf8';
             $table->collation = 'utf8_unicode_ci';
+            
         });
     }
 
