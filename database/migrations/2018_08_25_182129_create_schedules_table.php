c88f5eb6f064549f6e463ae0f3ba863879e66485
noureddine bensebia <nounoubensebia96@gmail.com>
2018-08-25 19:29:24 +0100

    adding otp path finder schedules

---
+++ database/migrations/2018_08_25_182129_create_schedules_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('line_id')->unsigned();
+            $table->foreign('line_id')->references('id')->on('lines')->onDelete('cascade');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->integer('days')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
