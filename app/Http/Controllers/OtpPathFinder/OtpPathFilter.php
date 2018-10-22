4ee6f3c072fa76f1d9c4d161441a53eb20cac170
nouno <nounoubensebia96@gmail.com>
2018-10-22 22:40:56 +0100

    fixing filter

--- app/Http/Controllers/OtpPathFinder/OtpPathFilter.php
+++ app/Http/Controllers/OtpPathFinder/OtpPathFilter.php
@@ -87,7 +87,7 @@ class OtpPathFilter
 
         foreach ($pathsMap as &$pathEntry)
         {
-            if ($pathEntry['path']->getPathDuration()>=$minDuration*2)
+            if ($pathEntry['path']->getPathDuration()>=$minDuration*3)
             {
                 $pathEntry['admissible'] = false;
             }
